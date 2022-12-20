5e70b90b539f11515ec77e7fcab7d4b291a76d78
Adrian Ardizza <tmgfaction@gmail.com>
2022-12-20 18:06:20 +0700

    feat(search): finished search/suggestion query

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request
 from flask_api import status
+from flask_cors import CORS
 
+import json
 import sparql
 
 app = Flask(__name__)
+
+CORS(app) # This will enable CORS for all routes
 
 @app.route("/search", methods=["GET"])
 def search():
@@ -17,4 +21,15 @@ def search():
     except:
         return "An error occurred while fetching your search results.", status.HTTP_500_BAD_REQUEST
 
+@app.route("/suggestions", methods=["GET"])
+def suggestions():
+    args = request.args
+    query = args.get("q")
 
+    if not query:
+        return json.dumps([])
+
+    try:
+        return sparql.get_suggestions(query)
+    except:
+        return "An erro occurred while fetching your search results.", status.HTTP_500_BAD_REQUEST
--- sparql.py
+++ sparql.py
@@ -1,23 +1,66 @@
 from SPARQLWrapper import JSON, SPARQLWrapper
+import json
 
 sparql = SPARQLWrapper(
-    "http://vocabs.ardc.edu.au/repository/api/sparql/"
-    "csiro_international-chronostratigraphic-chart_geologic-time-scale-2020"
+    "http://localhost:9999/blazegraph/namespace/kb/sparql"
 )
 
 sparql.setReturnFormat(JSON)
 
 def search(query: str):
     sparql.setQuery("""
-        PREFIX gts: <http://resource.geosciml.org/ontology/timescale/gts#>
+        prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+        prefix skos: <http://www.w3.org/2004/02/skos/core#>
+        prefix owl: <http://www.w3.org/2002/07/owl#>
+        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        prefix ex:    <http://example.org/data/>
+        prefix exv:   <http://example.org/vocab#>
+        prefix bds: <http://www.bigdata.com/rdf/search#>
 
-        SELECT *
-        WHERE {
-            ?a a gts:Age .
-        }
-        ORDER BY ?a
-        LIMIT 3
-        """
-    )
+        SELECT ?id ?title (GROUP_CONCAT(?genre_name;SEPARATOR=", ") AS ?genres) ?image WHERE {{
+          BIND({} AS ?query)
+          ?id a ex:AnimeID ;
+            exv:title ?title ;
+            exv:genres ?genre .
 
-    return sparql.queryAndConvert()
+          OPTIONAL {{
+            ?id exv:main_picture ?image .
+          }}
+
+          ?title bds:search ?query .
+          ?genre rdfs:label ?genre_name .
+        }} GROUP BY ?id ?title ?image
+    """.format(json.dumps(query)))
+    
+    return sparql.queryAndConvert()["results"]["bindings"]
+
+def get_suggestions(query: str):
+    sparql.setQuery("""
+        prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+        prefix skos: <http://www.w3.org/2004/02/skos/core#>
+        prefix owl: <http://www.w3.org/2002/07/owl#>
+        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        prefix ex:    <http://example.org/data/>
+        prefix exv:   <http://example.org/vocab#>
+        prefix bds: <http://www.bigdata.com/rdf/search#>
+
+        SELECT ?id ?title (GROUP_CONCAT(?genre_name;SEPARATOR=", ") AS ?genres) ?image WHERE {{
+          BIND({} AS ?query)
+          ?id a ex:AnimeID ;
+            exv:title ?title ;
+            exv:genres ?genre .
+
+          OPTIONAL {{
+            ?id exv:main_picture ?image .
+          }}
+
+          ?title bds:search ?query .
+          ?genre rdfs:label ?genre_name .
+        }} GROUP BY ?id ?title ?image LIMIT 4
+    """.format(json.dumps(query)))
+
+    return sparql.queryAndConvert()["results"]["bindings"]
+
+
